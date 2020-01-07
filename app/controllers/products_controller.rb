5409ee16b83ef6d30d272bf3306678d6b57f5389
Jason McBride <jmcbride.isye@gmail.com>
2020-01-07 21:43:23 +0000

    bug fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
 class ProductsController < ApplicationController
 
   def index
+
     @products = Product.first
 
-    @current_user_id = current_user.id
-    if Subscription.where(user_id: @current_user_id).exists?
-      @subscription = Subscription.where(user_id: @current_user_id)
+
+    if current_user != nil
+      @current_user_id = current_user.id
+      if Subscription.where(user_id: @current_user_id).exists?
+        @subscription = Subscription.where(user_id: @current_user_id)
+      else
+        @subscription = "none"
+      end
     else
       @subscription = "none"
     end
